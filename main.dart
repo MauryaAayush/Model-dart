65ad114873f37b5e6f509466286e9b7f2608293b
Admin <maayush235@gmail.com>
2024-06-10 11:46:44 +0530

    Radhey radhey

--- main.dart
+++ main.dart
@@ -1,7 +1,30 @@
 import 'model.dart';
+import 'data.dart';
 
 void main() {
   DataModel dataModel;
 
-  
+  for (int i = 0; i < info.length; i++) {
+    dataModel = DataModel.fromJson(info[i]);
+
+    print('-----------------------------');
+    print('id: ${dataModel.id}');
+    print('name: ${dataModel.name}');
+    print('username: ${dataModel.username}');
+    print('email: ${dataModel.email}');
+    print('address:');
+    print('\tstreet: ' + '${dataModel.address!.street}');
+    print('\tsuite: ' + '${dataModel.address!.suite}');
+    print('\tcity: ' + '${dataModel.address!.city}');
+    print('\tzipcode: ' + '${dataModel.address!.zipCode}');
+    print('\tgeo:');
+    print('\t\tlat: ' + '${dataModel.address!.geo!.lat}');
+    print('\t\tlng: ' + '${dataModel.address!.geo!.lng}');
+    print('phone: ${dataModel.phone}');
+    print('website: ${dataModel.website}');
+    print('company:');
+    print('\tname: ' + '${dataModel.company!.name}');
+    print('\tcatchPhrase: ' + '${dataModel.company!.catchPhrase}');
+    print('\tbs: ' + '${dataModel.company!.bs}');
+  }
 }
--- model.dart
+++ model.dart
@@ -1,3 +1,5 @@
+import 'data.dart';
+
 class DataModel {
   int? id;
   String? name;
@@ -75,5 +77,33 @@ class Company {
   factory Company.fromJson(Map json) {
     return Company(
         name: json['name'], catchPhrase: json['catchPhrase'], bs: json['bs']);
+  }
+}
+
+void main() {
+  DataModel dataModel;
+
+  for (int i = 0; i < info.length; i++) {
+    dataModel = DataModel.fromJson(info[i]);
+
+    print('-----------------------------');
+    print('id: ${dataModel.id}');
+    print('name: ${dataModel.name}');
+    print('username: ${dataModel.username}');
+    print('email: ${dataModel.email}');
+    print('address:');
+    print('\tstreet: ' + '${dataModel.address!.street}');
+    print('\tsuite: ' + '${dataModel.address!.suite}');
+    print('\tcity: ' + '${dataModel.address!.city}');
+    print('\tzipcode: ' + '${dataModel.address!.zipCode}');
+    print('\tgeo:');
+    print('\t\tlat: ' + '${dataModel.address!.geo!.lat}');
+    print('\t\tlng: ' + '${dataModel.address!.geo!.lng}');
+    print('phone: ${dataModel.phone}');
+    print('website: ${dataModel.website}');
+    print('company:');
+    print('\tname: ' + '${dataModel.company!.name}');
+    print('\tcatchPhrase: ' + '${dataModel.company!.catchPhrase}');
+    print('\tbs: ' + '${dataModel.company!.bs}');
   }
 }
